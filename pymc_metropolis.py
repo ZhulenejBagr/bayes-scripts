2a0382b819aba86c947edb033768be26b96c0475
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2023-10-17 23:26:11 +0200

    attempt at parallel sampling

--- pymc_metropolis.py
+++ pymc_metropolis.py
@@ -2,9 +2,9 @@ import pymc as pm
 import numpy as np
 import arviz as az
 import matplotlib.pyplot as plt
+import timeit as ti
 
-def metropolis(samples = 10000):
-    chains = 1
+def metropolis(samples = 10000, n_cores = 4, n_chains = 4):
     tune = 2000
     observed = -1e-3
     sigma = 1e-1
@@ -20,12 +20,39 @@ def metropolis(samples = 10000):
         # G by pak mohlo být f_(U|Y) (u|y), neboli ve figure 3.1d?
         G = pm.Normal('G', mu = G_mean, sigma = sigma, observed = observed)
 
-        trace = pm.sample(samples, tune = tune, step = pm.Metropolis(), chains = chains)
+        trace = pm.sample(samples, tune = tune, step = pm.Metropolis(), chains = n_chains, cores = n_cores)
     
     return trace
 
 
 if __name__ == "__main__":
-    trace = metropolis()
-    az.plot_pair(trace, kind="hexbin", marginals=True)
+    iters = 3
+    t1c1 = ti.timeit('metropolis(n_cores = 1, n_chains = 1)', globals = globals(), number = iters)
+    t4c1 = ti.timeit('metropolis(n_cores = 4, n_chains = 1)', globals = globals(), number = iters)
+    t16c1 = ti.timeit('metropolis(n_cores = 16, n_chains = 1)', globals = globals(), number = iters)
+    t1c4 = ti.timeit('metropolis(n_cores = 1, n_chains = 4)', globals = globals(), number = iters)
+    t4c4 = ti.timeit('metropolis(n_cores = 4, n_chains = 4)', globals = globals(), number = iters)
+    t16c4 = ti.timeit('metropolis(n_cores = 16, n_chains = 4)', globals = globals(), number = iters)
+    t1c16 = ti.timeit('metropolis(n_cores = 1, n_chains = 16)', globals = globals(), number = iters)
+    t4c16 = ti.timeit('metropolis(n_cores = 4, n_chains = 16)', globals = globals(), number = iters)
+    t16c16 = ti.timeit('metropolis(n_cores = 16, n_chains = 16)', globals = globals(), number = iters)
+    
+    print(f"Execution time for 1 chain:\n1 core: {t1c1}\n4 cores: {t4c1}\n16 cores: {t16c1}\n")
+    print(f"Execution time for 4 chains:\n1 core: {t1c4}\n4 cores: {t4c4}\n16 cores: {t16c4}\n")
+    print(f"Execution time for 16 chains:\n1 core: {t1c16}\n4 cores: {t4c16}\n16 cores: {t16c16}\n")
+    # Execution time for 1 chain:
+    #1 core: 11.148524099990027
+    #4 cores: 10.298176300013438
+    #16 cores: 10.456789700023364
+
+    #Execution time for 4 chains:
+    #1 core: 37.46264680000604
+    #4 cores: 86.86602760001551
+    #16 cores: 87.1835080999881
+
+    #Execution time for 16 chains:
+    #1 core: 147.5261680999829
+    #4 cores: 382.13164209999377
+    #16 cores: 610.7609780000057
+
     plt.show()
