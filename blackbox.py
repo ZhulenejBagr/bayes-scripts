80ac28cd47b306333ed7993a543ea9e17b1b249d
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2023-11-30 18:10:33 +0100

    Refactor blackbox

--- blackbox.py
+++ blackbox.py
@@ -1,11 +1,8 @@
 # https://www.pymc.io/projects/examples/en/latest/howto/blackbox_external_likelihood_numpy.html
 # https://www.pymc.io/projects/docs/en/latest/api/distributions/generated/pymc.CustomDist.html
-import pytensor as pt
 import numpy as np
-import numpy.random as npr
+import pytensor as pt
 from scipy.stats import norm, multivariate_normal
-
-generator = npr.Generator(npr.MT19937())
 
 def sample(mu, rng=None, size=None):
     sigma = sigma=np.array([[4, -2], [-2, 4]])
@@ -24,18 +21,3 @@ def log_likelihood(value, mu):
     #likelihood = likelihood * multivariate_normal(mu, sigma).pdf(value)
     return np.log(likelihood)
 
-
-
-class LogLike(pt.graph.Op):
-
-    itypes = [pt.tensor.vector]
-    otypes = [pt.tensor.scalar]
-
-    def __init__(self, loglike):
-        self.likelihood = loglike
-
-    def perform(self, node, inputs, outputs):
-        (theta,) = inputs
-
-        logl = self.likelihood(theta)
-        outputs[0][0] = np.array(logl)
