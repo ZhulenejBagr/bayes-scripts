b526eea3400b184758495e1877d541be1b6c3efd
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2023-11-30 18:10:21 +0100

    Fix log likelihood function of blackbox model

--- blackbox.py
+++ blackbox.py
@@ -13,11 +13,15 @@ def sample(mu, rng=None, size=None):
     return value
 
 def log_likelihood(value, mu):
-    sigma=np.array([[4, -2], [-2, 4]])
-    noise_sigma=2e-4
-    observed=-1e-3
-    likelihood = norm.pdf(value - observed, loc=0, scale=noise_sigma)
-    # likelihood = norm.pdf(value - observed, loc=0, scale=noise_sigma) * multivariate_normal(mu, sigma).pdf(value)
+    sigma = np.array([[4, -2], [-2, 4]])
+    noise_sigma = 2e-4
+    observed = -1e-3
+    #operator = np.multiply(-1 / 80, np.add(3 / np.exp(value[0]),  1 / np.exp(value[1])))
+    operator = -1 / 80 * (3 / np.exp(value[0]) + 1 / np.exp(value[1]))
+    v = np.subtract(operator, observed)
+    # likelihood = norm.pdf(v, loc=0, scale=noise_sigma)
+    likelihood = 1 / (noise_sigma * np.sqrt(2 * np.pi)) * np.exp(-1 / 2 * np.power(v / noise_sigma, 2))
+    #likelihood = likelihood * multivariate_normal(mu, sigma).pdf(value)
     return np.log(likelihood)
 
 
