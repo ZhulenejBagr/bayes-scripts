b178311c8532a9e1f2bcc4821230ae137a9f9ee7
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2023-12-04 22:03:48 +0100

    rewrite blackbox model

--- blackbox.py
+++ blackbox.py
@@ -1,23 +1,40 @@
 # https://www.pymc.io/projects/examples/en/latest/howto/blackbox_external_likelihood_numpy.html
 # https://www.pymc.io/projects/docs/en/latest/api/distributions/generated/pymc.CustomDist.html
 import numpy as np
-import pytensor as pt
-from scipy.stats import norm, multivariate_normal
+
+external_generator = np.random.Generator(np.random.MT19937())
+
 
 def sample(mu, rng=None, size=None):
     sigma = sigma=np.array([[4, -2], [-2, 4]])
-    value = rng.multivariate_normal(mean=mu, cov=sigma, size=size)
+    value = external_generator.multivariate_normal(mean=mu, cov=sigma, size=size)
     return value
 
-def log_likelihood(value, mu):
+# norm pdf with m=0
+def norm_pdf(value, sigma, mu=0):
+    return 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-1 / 2 * np.power((value - mu) / sigma, 2))
+
+# bivariate normal distribution pdf
+def bvn_pdf(value, mu, sigma):
+    k = 2
+    return (2 * np.pi) ** (k) * np.linalg.det(sigma) ** (-1 / 2) * np.exp(-1 / 2 * np.transpose(value - mu) * np.linalg.inv(sigma) * (value - mu))
+
+def observation_operator(value):
+    return -1 / 80 * (3 / np.exp(value[0]) + 1 / np.exp(value[1]))
+
+def log_probability(value, mu):
     sigma = np.array([[4, -2], [-2, 4]])
     noise_sigma = 2e-4
     observed = -1e-3
     #operator = np.multiply(-1 / 80, np.add(3 / np.exp(value[0]),  1 / np.exp(value[1])))
-    operator = -1 / 80 * (3 / np.exp(value[0]) + 1 / np.exp(value[1]))
-    v = np.subtract(operator, observed)
-    # likelihood = norm.pdf(v, loc=0, scale=noise_sigma)
-    likelihood = 1 / (noise_sigma * np.sqrt(2 * np.pi)) * np.exp(-1 / 2 * np.power(v / noise_sigma, 2))
+    numerator_operator = observation_operator(value)
+    numerator_mu = np.subtract(numerator_operator, observed)
+    numerator = norm_pdf(numerator_mu, noise_sigma) * bvn_pdf(value, mu, sigma)
+
+    denominator_operator = observation_operator(mu)
+    denominator_mu = np.subtract(denominator_operator, observed)
+    denominator = norm_pdf(denominator_mu, sigma) * bvn_pdf(mu, value, sigma)
+
     #likelihood = likelihood * multivariate_normal(mu, sigma).pdf(value)
-    return np.log(likelihood)
+    return np.log(np.divide(numerator / denominator))
 
