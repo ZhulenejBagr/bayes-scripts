8e3e1285fed03e90110323753a7036fbc10a1f7f
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2023-12-19 22:38:06 +0100

    Change generator params

--- idata_generator.py
+++ idata_generator.py
@@ -1,9 +1,11 @@
 import numpy as np
 from pymc_metropolis import generate_idata_sets
 # idata is saved to folder /idata relative to this script's directory
+# all heavy lifting is done by the generate_idata_sets method
+# 
 
 def generate_standard_data_sets():
-    samples = 20000
+    samples = 10000
     tune = 20000
     prior_mean = np.array([5, 3])
     prior_cov = np.array([[4, -2],[-2, 4]])
@@ -15,10 +17,10 @@ def generate_standard_data_sets():
     generate_idata_sets(samples=samples, tune=tune, prior_mean=prior_mean, prior_cov=prior_cov, prefix=prefix)
 
 def generate_offset_data_sets():
-    samples = 20000
+    samples = 10000
     tune = 20000
-    prior_mean = np.array([6, 4])
-    prior_cov = np.array([[8, -2],[-2, 8]])
+    prior_mean = np.array([7, 4.2])
+    prior_cov = np.array([[10, -5],[-5, 10]])
     prefix = "offset"
     print("--------------------------------")
     print("Generating offset data sets...")
