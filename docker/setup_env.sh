cbfd2ee9090f25d52564eea435b04778f470c055
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2023-12-19 23:10:10 +0100

    Add change directory at the start and end of script

--- docker/setup_env.sh
+++ docker/setup_env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cd /
 python3.8 -m venv /home/flow/.virtualenvs/bruh
 source /home/flow/.virtualenvs/bruh/bin/activate
 pip3 install wheel
 pip3 install -r /mnt/docker/requirements.txt
 deactivate
 ln -s /home/flow/.virtualenvs/bruh/bin/activate /home/flow/activate_venv
+cd /home/flow
