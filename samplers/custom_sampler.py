480e197c354595bffaba6f40bc0d70bc9da567be
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-02-14 12:42:55 +0100

    Refactor custom sampler

--- samplers/custom_sampler.py
+++ samplers/custom_sampler.py
@@ -1,14 +1,14 @@
-from math import pi
 import numpy as np
 import numpy.random as npr
-from scipy.stats import multivariate_normal
+from scipy.stats import multivariate_normal, norm
 import arviz as az
+from samplers.idata_tools import save_idata_to_file
 
 generator = npr.Generator(npr.MT19937())
 
-# Simple metropolis-hastings implementation for one variable
+# Simple metropolis-hastings implementation
 def norm_pdf(value, mean, std):
-    return 1 / (std * np.sqrt(2 * pi)) * np.exp(-1 / 2 * np.power((value - mean) / std, 2))
+    return norm(loc=mean, scale=std).pdf(value)
 
 def mvn_pdf(rnd, mean, cov):
     return multivariate_normal.pdf(rnd, mean, cov)
@@ -24,50 +24,52 @@ def sample_prior(samples=10000, mean=np.array([5,3]), cov=np.array([[4,-2],[-2,4
 
 def metropolis(
         samples=10000,
-        tune=5000,
-        prior_mean=np.array([5,3]),
+        tune=3000,
+        prior_mean=np.array([5, 3]),
         prior_sigma=np.array([[4, -2], [-2, 4]])):
+    print(f"Sampling {samples} samples with {tune} tune")
     # set up result array
     total_samples = samples + tune
     sampled = {"U": np.zeros((0, 2)), "G": np.zeros(0)}
     likelihood = np.zeros(0)
     # prior
     prior_pdf = lambda rnd: mvn_pdf(rnd, prior_mean, prior_sigma)
-    prior_candidate = lambda mean: generator.multivariate_normal([mean[0], mean[1]], prior_sigma)
+    candidate_sample = lambda mean: generator.multivariate_normal([mean[0], mean[1]], prior_sigma)
     # posterior
-    posterior_std = 2e-4
-    posterior_operator = lambda u: -1 / 80 * (3 / np.exp(u[0]) + 1 / np.exp(u[1]))
-    observed_value = -1e-3
-    posterior_pdf = lambda rnd: norm_pdf(rnd - observed_value, 0, posterior_std)
+    noise_sigma = 2e-4
+    noise_operator = lambda u: -1 / 80 * (3 / np.exp(u[0]) + 1 / np.exp(u[1]))
+    observed = -1e-3
+    noise_pdf = lambda rnd: norm_pdf(rnd - observed, 0, noise_sigma)
     #adj_cov = np.multiply(2 * np.pi, prior_sigma)
     #factor = np.sqrt(np.linalg.det(adj_cov))
 
     # sampling process
     accepted = 0
-    g = 0
-    u = prior_mean
+    current = prior_mean
+    current_operator = noise_operator(current)
     while accepted < total_samples:
         # get U candidate sample
-        u_candidate = prior_candidate(u)
-        g_candidate = posterior_operator(u_candidate)
+        candidate = candidate_sample(current)
+        candidate_operator = noise_operator(candidate)
 
         # accept/reject candidates
-        u_current_probability = prior_pdf(u)
-        u_candidate_probability = prior_pdf(u_candidate)
+        current_prior = prior_pdf(current)
+        candidate_prior = prior_pdf(candidate)
 
-        g_candidate_probability = posterior_pdf(g_candidate)
-        g_current_probability = posterior_pdf(g)
-        threshold = min([1, g_candidate_probability / g_current_probability * u_candidate_probability / u_current_probability])
+        current_noise = noise_pdf(current_operator)
+        candidate_noise = noise_pdf(candidate_operator)
+        threshold = min([1, candidate_noise / current_noise * candidate_prior / current_prior])
 
         random_probability = npr.uniform()
 
         if random_probability < threshold:
-            sampled["U"] = np.concatenate((sampled["U"], u_candidate.reshape(1, 2)), axis=0)
-            sampled["G"] = np.append(sampled["G"], g_candidate)
-            likelihood = np.append(likelihood, np.log(g_candidate_probability))
-            u = u_candidate
-            g = g_candidate
+            sampled["U"] = np.concatenate((sampled["U"], candidate.reshape(1, 2)), axis=0)
+            sampled["G"] = np.append(sampled["G"], candidate_operator)
+            likelihood = np.append(likelihood, np.log(candidate_noise))
+            current = candidate
+            current_operator = candidate_operator
             accepted += 1
+            print(accepted)
         else:
             continue
 
@@ -103,3 +105,18 @@ def metropolis(
     idata.extend(prior_idata)
 
     return idata
+
+def sample_standard():
+    idata = metropolis(tune=20000, samples=10000)
+    save_idata_to_file(idata, filename="standard.custom_MH.idata")
+
+def sample_offset():
+    idata = metropolis(
+        tune=20000,
+        samples=10000,
+        prior_mean = np.array([7, 5]),
+        prior_sigma = np.array([[12, -6],[-6, 12]]))
+    save_idata_to_file(idata, filename="offset.custom_MH.idata")
+
+if __name__ == "__main__":
+    sample_offset()
