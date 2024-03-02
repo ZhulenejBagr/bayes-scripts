9b40af534d8361ce409e3ea78eb41bc5b6d010c7
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-03-02 20:52:35 +0100

    Add temporary static commit rev hash

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -2,6 +2,7 @@
 set -x
 
 git_commit=`git rev-parse --short=6 HEAD`
+git_commit="09da55" # TEMP static commit rev hash to avoid collisions
 tag=flow123d/bp_simunek:${1:-${git_commit}}
 
 # add --no-cache to clean build
