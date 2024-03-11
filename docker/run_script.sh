67787f589c2915a159a5d6a1508526ffa0dc772a
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-03-11 20:10:06 +0100

    Remove deprecated files

--- docker/run_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/flow
-sed -i -e 's/\r$//' /mnt/docker/setup_env.sh #(pokud nejde zanout setup_env.sh)
-/mnt/docker/setup_env.sh
-# run pymc stuff
-source activate_pymc
-sed -i -e 's/\r$//' /mnt/scripts/generate_and_plot.sh
-/mnt/scripts/generate_and_plot.sh
