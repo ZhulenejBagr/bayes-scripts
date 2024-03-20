5571b34d27a379410cdb00573d5ebd38d644bf06
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-03-20 16:05:12 +0100

    More pbs script changes

--- scripts/pbs_singularity_shell.sh
+++ scripts/pbs_singularity_shell.sh
@@ -6,6 +6,7 @@
 #PBS -l select=1:ncpus=1:mem=2gb 
 #PBS -l walltime=32:00:00
 
-./bin/fterm_sing
-source venv/bin/activate
-python -m bp_simunek.tests.simulation.test_tinyda_simulation
+# run fterm sing atleast once to create the image
+#./bin/fterm_sing
+
+singularity exec bp_simunek.sif ./scripts/singularity_run_script.sh
