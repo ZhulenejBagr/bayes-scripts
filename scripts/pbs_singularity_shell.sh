15b3b0f1ade6d9d842e4bf1d4bae62bc68d4933b
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-03-20 15:48:48 +0100

    Update charon pbs script

--- scripts/pbs_singularity_shell.sh
+++ scripts/pbs_singularity_shell.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#PBS -I
 #PBS -S /bin/bash
 #PBS -N interactive_singularity_shell
 #PBS -q charon
-#PBS -l select=1:ncpus=1:mem=5gb 
-#PBS -l walltime=02:00:00
+#PBS -l select=1:ncpus=1:mem=2gb 
+#PBS -l walltime=32:00:00
 
 ./bin/fterm_sing
+source venv/bin/activate
+python -m bp_simunek.tests.simulation.test_tinyda_simulation
