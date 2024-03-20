2265d22b8e040ba66298e4658de2ad06a552fef7
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-03-20 16:36:23 +0100

    Fix pbs script

--- scripts/pbs_singularity_shell.sh
+++ scripts/pbs_singularity_shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -S /bin/bash
-#PBS -N interactive_singularity_shell
+#PBS -N singularity_sample_script_1000
 #PBS -q charon
 #PBS -l select=1:ncpus=1:mem=2gb 
 #PBS -l walltime=32:00:00
@@ -9,4 +9,6 @@
 # run fterm sing atleast once to create the image
 #./bin/fterm_sing
 
-singularity exec bp_simunek.sif ./scripts/singularity_run_script.sh
+cd $PBS_O_WORKDIR
+
+singularity exec bp_simunek.sif bash scripts/singularity_run_script.sh
