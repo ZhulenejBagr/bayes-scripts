d9137a260403dee8a6bf4d497bce1d73119b4201
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-04-02 11:46:10 +0200

    Add another idata and change plot script

--- src/bp_simunek/plotting/flow_plots.py
+++ src/bp_simunek/plotting/flow_plots.py
@@ -4,7 +4,7 @@ from bp_simunek.samplers.idata_tools import read_idata_from_file
 from bp_simunek.plotting.plotting_tools import save_plot
 
 if __name__ == "__main__":
-    idata = read_idata_from_file("10x500_independence_0.idata")
+    idata = read_idata_from_file("10x500_independence_1.idata")
     axes = az.plot_posterior(idata)
     print(az.summary(idata))
-    save_plot("10x500_independence_posterior.pdf")
+    save_plot("10x500_independence_1_posterior.pdf")
