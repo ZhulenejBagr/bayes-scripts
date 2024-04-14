f11113e4de537b4d28e06ee32b4d2efe0274b27b
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-04-14 13:20:08 +0200

    Move params to config and adapt tests

--- tests/simulation/test_tinyda_simulation.py
+++ tests/simulation/test_tinyda_simulation.py
@@ -1,59 +1,38 @@
 import os
 from pathlib import Path
 import logging
+
 import arviz as az
 import pytest
 import ray
-from definitions import ROOT_DIR
 
-from bp_simunek.simulation.flow_wrapper import Wrapper
+from definitions import ROOT_DIR
+from bp_simunek.simulation.flow_wrapper import Wrapper, setup_config
 from bp_simunek.samplers.tinyda_flow import TinyDAFlowWrapper
 from bp_simunek.samplers.idata_tools import save_idata_to_file
 
-
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-
-@pytest.mark.skip
-def test_simulation3_with_tinyda():
-    os.chdir(script_dir)
-    # flow setup
-    workdir = Path("test_workdir3").absolute()
-    solver_id = 42
-    wrap = Wrapper(solver_id, workdir)
-    wrap.sim._config["measured_data_dir"] = os.path.join(script_dir, "../measured_data")
-
-    # tinyda + flow123 wrapper
-    n_chains = 1
-    tinyda_wrapper = TinyDAFlowWrapper(wrap)
-
-    # sampling process
-    sample_count = 5
-    idata = tinyda_wrapper.sample(sample_count=sample_count)
-    assert idata
-    assert idata["posterior"].sizes["draw"] == sample_count
-    logging.info("\n%s", str(az.summary(idata)))
-    save_idata_to_file(idata, filename="flow.sim3.idata")
-
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_simulation11_with_tinyda():
     #ray.init(runtime_env={"working_dir": ROOT_DIR})
     os.chdir(script_dir)
-    workdir = Path("test_workdir11").absolute()
+    output_dir = Path(ROOT_DIR, "output", "test11").absolute()
+    work_dir = Path("templates", "test_workdir11").absolute()
+    config = setup_config(work_dir)
     solver_id = 42
-    wrap = Wrapper(solver_id, workdir)
+    wrap = Wrapper(solver_id, output_dir, config)
     wrap.sim._config["measured_data_dir"] = os.path.join(script_dir, "../measured_data")
 
     # tinyda + flow123 wrapper
     n_chains = 1
     tinyda_wrapper = TinyDAFlowWrapper(wrap)
 
-    sample_count = 10
-    idata = tinyda_wrapper.sample(sample_count=sample_count, chains=n_chains)
+    idata = tinyda_wrapper.sample()
     assert idata
-    assert idata["posterior"].sizes["draw"] == sample_count
+    assert idata["posterior"].sizes["draw"] == tinyda_wrapper.sample_count
     logging.info("\n%s", str(az.summary(idata)))
-    save_idata_to_file(idata, filename="flow.sim11.idata")
+    save_idata_to_file(idata, folder_path=output_dir, filename="flow.sim11.idata")
 
 @pytest.mark.skip
 def test_simulation11_simplified_with_tinyda():
@@ -68,14 +47,14 @@ def test_simulation11_simplified_with_tinyda():
     n_chains = 1
     tinyda_wrapper = TinyDAFlowWrapper(wrap)
 
-    sample_count = 10
-    idata = tinyda_wrapper.sample(sample_count=sample_count, chains=n_chains)
+    sample_count = 5
+    idata = tinyda_wrapper.sample()
     assert idata
     assert idata["posterior"].sizes["draw"] == sample_count
     logging.info("\n%s", str(az.summary(idata)))
     save_idata_to_file(idata, filename="flow.sim11.idata")
 
-#@pytest.mark.skip
+@pytest.mark.skip
 def test_simulation11_with_tinyda_parallel():
     os.chdir(script_dir)
     workdir = Path("test_workdir11").absolute()
@@ -84,18 +63,17 @@ def test_simulation11_with_tinyda_parallel():
     wrap.sim._config["measured_data_dir"] = os.path.join(script_dir, "../measured_data")
 
     # tinyda + flow123 wrapper
-    n_chains = 4
     tinyda_wrapper = TinyDAFlowWrapper(wrap)
 
     sample_count = 5
-    idata = tinyda_wrapper.sample(sample_count=sample_count, chains=n_chains)
+    idata = tinyda_wrapper.sample(sample_count, False)
     assert idata
     assert idata["posterior"].sizes["draw"] == sample_count
     logging.info("\n%s", str(az.summary(idata)))
     save_idata_to_file(idata, filename="flow.sim11.idata")
 
 @pytest.mark.skip
-def sample11(sample_count=500, idata_name="flow_tinyda_1000.idata"):
+def sample11(idata_name="flow_tinyda_1000.idata"):
     # probably not the best solution
     # 107 char limit for socket path
     tmp_dir_symlink = os.path.join(os.path.expanduser("~"), ".r")
@@ -113,7 +91,7 @@ def sample11(sample_count=500, idata_name="flow_tinyda_1000.idata"):
     n_chains = 10
     tinyda_wrapper = TinyDAFlowWrapper(wrap)
 
-    idata = tinyda_wrapper.sample(sample_count=sample_count, chains=n_chains)
+    idata = tinyda_wrapper.sample()
     logging.info("\n%s", str(az.summary(idata)))
     save_idata_to_file(idata, filename=idata_name)
 
