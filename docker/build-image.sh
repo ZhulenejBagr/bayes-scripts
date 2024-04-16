ae0fa5d4e6ef34a15fbadef5a9f86cfd285a3990
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-04-17 01:55:30 +0200

    Change docker image name to different repo

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -3,10 +3,10 @@ set -x
 
 git_commit=`git rev-parse --short=6 HEAD`
 git_commit="09da55" # TEMP static commit rev hash to avoid collisions
-tag=flow123d/bp_simunek:${1:-${git_commit}}
+tag=ondrejsimunek/bpr:${1:-${git_commit}}
 
 # add --no-cache to clean build
 docker build --tag ${tag} .
 
 # can be pushed to Docker hub later..
-#docker push ${tag}
+docker push ${tag}
