3617844a2bdea33c60d64bdf53d2c6e2b98bb32e
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-04-18 17:45:59 +0200

    Change scripts to fit new sampling params

--- scripts/pbs_singularity_shell.sh
+++ scripts/pbs_singularity_shell.sh
@@ -3,8 +3,8 @@
 #PBS -S /bin/bash
 #PBS -N singularity_sample_script_500
 #PBS -q charon
-#PBS -l select=1:ncpus=10:mem=20gb:scratch_local=5gb
-#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=10:mem=10gb:scratch_local=5gb
+#PBS -l walltime=36:00:00
 
 # run fterm sing atleast once to create the image
 #./bin/fterm_sing
--- scripts/singularity_run_script.sh
+++ scripts/singularity_run_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source venv/bin/activate
-python -m tests.simulation.test_tinyda_simulation
+python -m bp_simunek.scripts.mlda_sample
