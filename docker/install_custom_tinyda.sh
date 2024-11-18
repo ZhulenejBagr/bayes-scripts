d05e5b6834243a2e8545dacdef920e9d8bec9bfe
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-11-18 17:02:20 +0100

    Add some venv scripts

---
+++ docker/install_custom_tinyda.sh
@@ -0,0 +1,15 @@
+env_path=$1
+tda_path=$2
+
+cdir="$(pwd)"
+
+cd $cdir
+source ${env_path}bin/activate && \
+mkdir ${tda_path} && \
+cd ${tda_path}
+git clone https://github.com/bagr-sus/tinyDA && \
+cd tinyDA && \
+git checkout shared-archive-proposals && \
+#python -m pip install -e . #editable
+python -m pip install . #non-editable
+cd $cdir
