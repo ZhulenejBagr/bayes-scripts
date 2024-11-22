d4ef9ff49d7203179fbe3ad3402a605a4cab0722
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-11-23 00:14:59 +0100

    Setup flow_plots for new prior

--- src/bp_simunek/plotting/flow_plots.py
+++ src/bp_simunek/plotting/flow_plots.py
@@ -125,15 +125,15 @@ def generate_all_flow_plots(idata, folder):
     ]
 
     prior_stds = [
-        2,
+        3,
+        0.7,
         0.5,
-        0.3,
-        0.3,
+        0.5,
         #0.3,
-        2.0,
-        9,
-        0.1,
-        0.1
+        3.0,
+        14,
+        0.15,
+        0.15
     ]
 
     for x, axrow in enumerate(axes):
