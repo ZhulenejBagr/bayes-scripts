1fb297125eaa64574d209e2c496ef79f164f207d
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-11-23 00:19:10 +0100

    Get rid of unused chars

--- docker/install_custom_tinyda.sh
+++ docker/install_custom_tinyda.sh
@@ -4,12 +4,12 @@ tda_path=$2
 cdir="$(pwd)"
 
 cd $cdir
-source ${env_path}bin/activate && \
-mkdir ${tda_path} && \
+source ${env_path}bin/activate
+mkdir ${tda_path}
 cd ${tda_path}
-git clone https://github.com/bagr-sus/tinyDA && \
-cd tinyDA && \
-git checkout shared-archive-proposals && \
+git clone https://github.com/bagr-sus/tinyDA
+cd tinyDA
+git checkout shared-archive-proposals
 #python -m pip install -e . #editable
 python -m pip install . #non-editable
 cd $cdir
