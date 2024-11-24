0e9a54f8e149c73e805e8262879bef51399712d3
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2024-11-24 20:25:44 +0100

    bruh

--- src/bp_simunek/simulation/flow123d_simulation.py
+++ src/bp_simunek/simulation/flow123d_simulation.py
@@ -257,12 +257,11 @@ class Flow123dSimulation:
         data = observe_dict['data']
         logging.warning(data)
         data_values = np.array([d[field_name] for d in data])
-        logging.warning(data_values)
-        #values = data_values[:, points2collect_indices]
+        #logging.warning(data_values)
+        values = data_values[:, points2collect_indices]
         obs_times = np.array([d["time"] for d in data]).transpose()
         logging.warning(obs_times)
         logging.warning(select_times)
-
         if select_times is not None:
             # check that observe data are computed at all times of defined time axis
             all_times_computed = np.all(np.isin(select_times, obs_times))
